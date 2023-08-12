120131ec6ddc50ab97e9fd46758cb4b407e4e15e
Selim Kentel <selimkentel@gmail.com>
2023-08-12 02:21:52 +0200

    opt

--- liteapi/src/lib.rs
+++ liteapi/src/lib.rs
@@ -2,17 +2,18 @@ mod app;
 mod types;
 mod utils;
 
-pub mod http;
-pub(crate) mod openapi;
-
 pub use simple_async::async_main as entry;
+pub use futures_executor::block_on as execute;
 pub use self::app::LiteAPI;
+pub mod http;
 
 pub(crate) use self::types::{Handler, Routes};
 pub(crate) use self::utils::*;
+pub(crate) mod openapi;
 
 pub use serde_json::json;
 pub use serde_json::to_string as json2string;
+
 use std::fs;
 use std::io::Error;
 
